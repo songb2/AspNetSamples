6256164860b1400825c51cfc568f26dc1ae82924
songb2 <brucesong2010@hotmail.com>
2014-04-03 00:40:46 +0800

    Trace sample

---
+++ TraceDemo/TestTrace.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestTrace.aspx.cs" Inherits="TraceDemo.TestTrace" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btnWriteLog" runat="server" Text="WriteLog" OnClick="btnWriteLog_Click" />
+        <asp:Button ID="btnViewLog" runat="server" Text="View EventLog" OnClick="btnViewLog_Click" />
+    </div>
+        <p>
+            &nbsp;</p>
+        <asp:GridView ID="LogGrid" runat="server" AllowPaging="True" BackColor="White" BorderColor="#CC9966" BorderStyle="None" BorderWidth="1px" CellPadding="4" OnPageIndexChanging="LogGrid_PageIndexChanging" PageSize="20">
+            <FooterStyle BackColor="#FFFFCC" ForeColor="#330099" />
+            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="#FFFFCC" />
+            <PagerStyle BackColor="#FFFFCC" ForeColor="#330099" HorizontalAlign="Center" />
+            <RowStyle BackColor="White" ForeColor="#330099" />
+            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="#663399" />
+            <SortedAscendingCellStyle BackColor="#FEFCEB" />
+            <SortedAscendingHeaderStyle BackColor="#AF0101" />
+            <SortedDescendingCellStyle BackColor="#F6F0C0" />
+            <SortedDescendingHeaderStyle BackColor="#7E0000" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ TraceDemo/TestTrace.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TraceDemo
+{
+    public partial class TestTrace : System.Web.UI.Page
+    {
+        // Need to run visual studio as administrator
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindDataGrid();
+            }
+        }
+
+        protected void btnWriteLog_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.TraceError("btnWriteLog_Click method called");
+        }
+
+        protected void btnViewLog_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Trace.TraceError("Page_Load method called");
+        }
+
+        protected void LogGrid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            LogGrid.PageIndex = e.NewPageIndex;
+            BindDataGrid();
+        }
+
+        void BindDataGrid()
+        {
+            EventLog log = new EventLog();
+            log.Log = "Application";
+            log.MachineName = ".";
+            LogGrid.DataSource = log.Entries;
+            LogGrid.DataBind();
+        }
+
+        // sort not work, need to do again  
+        protected void LogGrid_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            DataTable m_DataTable = LogGrid.DataSource as DataTable;
+
+            if (m_DataTable != null)
+            {
+                DataView m_DataView = new DataView(m_DataTable);
+                m_DataView.Sort = e.SortExpression + " " + e.SortDirection;
+
+                LogGrid.DataSource = m_DataView;
+                LogGrid.DataBind();
+            }
+        }
+    }
+}
